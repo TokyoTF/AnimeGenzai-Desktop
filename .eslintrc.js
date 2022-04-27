621d3a0afe70bdc4a9d4ac7b1bbc43ad36c81666
TokyoTF <TokyoTF@hotmail.com>
2022-04-26 22:23:32 -0400

    WorkFlow

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
-        "node": true
+        "node": true,
+        "jest/globals": true
       },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
+    "plugins": ["jest"],
     "rules": {
     }
 }
